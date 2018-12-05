8e914af9fd890d6afe491c589bac09f2064022c7
Lucas Landim <jlucaslandim@gmail.com>
2018-12-05 20:47:17 -0300

    Caelum - Java e Orientação a Objetos, Capitulo - 02 e 03

---
+++ lista-de-exercicios-02/Capitulo-02/MeuPrograma2.java
@@ -0,0 +1,9 @@
+class MeuPrograma2{
+
+	public static void main(String[] args) {
+	
+	System.out.println ("texto2texto2texto2texto2texto2texto2 ");
+	System.out.println ("texto2texto2texto2texto2texto2texto2 ");
+
+	}
+}
---
+++ lista-de-exercicios-02/Capitulo-02/MeuPrograma3.java
@@ -0,0 +1,7 @@
+class MeuPrograma3{
+
+	public static void main(String[] args) {
+	
+	System.out.println ("texto3texto3texto3\ntexto3texto3texto3 ");
+	}
+}
